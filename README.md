2dbb7fb8bef9374ee42c9a1c7357417caf8fba43
ttzshawn <ttzshawn@gmail.com>
2017-08-06 17:59:02 +0800

    [Feature] Initialize repo

---
+++ README.md
@@ -0,0 +1,28 @@
+# vue-custom-keyboard-example
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## Demo
+
+Show case below.
+
+![Show case 1](https://github.com/ttzshawn/vue-custom-keyboard-demo/keyboard-demo-1.png)
+![Show case 2](https://github.com/ttzshawn/vue-custom-keyboard-demo/keyboard-demo-2.png)
---
+++ src/components/Demo.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h2>Vue Custom Keyboard Example</h2>
+    <h1 class="title"  @click="showKeyboard">{{ inputValue }}</h1>
+    <VueCustomKeyboard
+      :isOpen="isOpen"
+      :onChange="onChange"
+      :onBlur="onBlur"
+      :onDone="onDone"
+    >
+    </VueCustomKeyboard>
+  </div>
+</template>
+
+<script>
+import VueCustomKeyboard from 'vue-custom-keyboard'
+import 'vue-custom-keyboard/dist/vue-custom-keyboard.min.css'
+
+export default {
+  name: 'demo',
+  components: {
+    VueCustomKeyboard
+  },
+  data () {
+    return {
+      inputValue: '',
+      isOpen: true
+    }
+  },
+  methods: {
+    onChange (value) {
+      this.inputValue = value
+    },
+    onBlur () {
+      this.isOpen = false
+    },
+    onDone () {
+      this.isOpen = false
+    },
+    showKeyboard () {
+      this.isOpen = true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title {
+  width: 200px;
+  height: 44px;
+  margin: 0 auto;
+  text-align: center;
+  border: 1px solid lightblue;
+}
+</style>
